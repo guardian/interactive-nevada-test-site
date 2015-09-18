1f81bc9be8a6ec63fa5e6941d3f60f858f2131b4
Will Franklin <will.franklin@guardian.co.uk>
2015-09-18 15:25:50 +0100

    Add bandwidth tests

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import reqwest from 'reqwest'
 import doT from 'olado/doT'
 import share from './lib/share'
+import testBandwidth from './lib/test-bandwidth'
 
 import video from './components/video'
 import map from './components/map'
@@ -22,25 +23,34 @@ const baseComponent = type => (el, options) => el.innerHTML += templates[type](o
 
 var shareFn = share('Interactive title', 'http://gu.com/p/URL', '#Interactive');
 
+function app(bitrate, el, config, resp) {
+    config.bitrate = bitrate;
+    el.innerHTML = '';
+    resp.blocks.forEach(block => {
+        block.config = config;
+        if (block.copy) {
+            block.copy = block.copy.replace(/[\r\n]+/g, '\n').trim().split('\n');
+        }
+        var componentEl = document.createElement('section');
+        if (block.darkcolor) {
+            componentEl.style.backgroundColor = block.darkcolor;
+        }
+        el.appendChild(componentEl);
+        (components[block.block] || baseComponent(block.block))(componentEl, block);
+    });
+}
+
 export function init(el, context, config, mediator) {
     reqwest({
         url: contentURL,
         type: 'json',
         crossOrigin: true,
         success: resp => {
-            el.innerHTML = '';
-            resp.blocks.forEach(block => {
-                block.config = config;
-                if (block.copy) {
-                    block.copy = block.copy.replace(/[\r\n]+/g, '\n').trim().split('\n');
-                }
-                var componentEl = document.createElement('section');
-                if (block.darkcolor) {
-                    componentEl.style.backgroundColor = block.darkcolor;
-                }
-                el.appendChild(componentEl);
-                (components[block.block] || baseComponent(block.block))(componentEl, block);
-            });
+            if (window.innerWidth > 600) {
+                testBandwidth(bitrate => app(bitrate, el, config, resp));
+            } else {
+                app("500", el, config, resp);
+            }
         }
     });
 
