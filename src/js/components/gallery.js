b5fea49b6492a1f1a01745e14e9fe77d445f26b4
Will Franklin <will.franklin@guardian.co.uk>
2015-09-20 19:07:50 +0100

    Gallery

--- src/js/components/gallery.js
+++ src/js/components/gallery.js
@@ -5,4 +5,30 @@ import template from './templates/gallery.html!text'
 const templateFn = doT.template(template);
 export default function (el, options) {
     el.innerHTML = templateFn(options);
+
+    var photoEls = [].slice.call(el.querySelectorAll('.js-photo'));
+    var prevEl = el.querySelector('.js-prev');
+    var nextEl = el.querySelector('.js-next');
+    var photoI = 0;
+
+    function show(i) {
+        if (i >= 0 && i < photoEls.length) {
+            photoEls[photoI].removeAttribute('data-visible');
+            photoEls[i].setAttribute('data-visible', '');
+
+            prevEl.disabled = i === 0;
+            nextEl.disabled = i === photoEls.length - 1;
+
+            photoI = i;
+        }
+    }
+
+    prevEl.addEventListener('click', () => show(photoI - 1));
+    nextEl.addEventListener('click', () => show(photoI + 1));
+
+    show(photoI);
+
+    if (window.innerWidth > 600) {
+        sticky(el, el.querySelector('.js-sticky'));
+    }
 }
